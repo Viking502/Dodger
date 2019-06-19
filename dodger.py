eb71c360e9fb98bc4781a8d87c88052c4782ced7
Viking502 <50868501+Viking502@users.noreply.github.com>
2019-06-19 19:28:05 +0200

    Add files via upload

--- dodger.py
+++ dodger.py
@@ -1,10 +1,9 @@
 import pygame
-from pygame import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-import math as mat
-# import numpy as np
-import random
+import math
+import numpy as np
+
 
 class Tunel:
 
@@ -12,7 +11,7 @@ class Tunel:
         self.radius = 42
         self.sides_num = 12
 
-        self.depth = 30
+        self.depth = 15
         self.interval = 30
 
         self.vertices = []
@@ -22,9 +21,9 @@ class Tunel:
     def build(self):
         for z in range(0, self.depth):
             for i in range(0, self.sides_num):
-                self.vertices.append([int(mat.sin(2 * mat.pi / self.sides_num * i) * self.radius),
-                                      int(mat.cos(2 * mat.pi / self.sides_num * i) * self.radius) + self.radius - 20, z * self.interval])
-
+                self.vertices.append([int(math.sin(2 * math.pi / self.sides_num * i) * self.radius),
+                                      int(math.cos(2 * math.pi / self.sides_num * i) * self.radius), z * self.interval])
+#  + self.radius - 20
         for z in range(0, self.depth):
             for i in range(0, self.sides_num):
                 self.edges.append([i + z * self.sides_num, ((i + 1) % self.sides_num) + z * self.sides_num])
@@ -43,11 +42,6 @@ class Tunel:
 
         for side in self.sides:
             for vertex in side:
-
-                # r = random.random()
-                # g = random.random()
-                # b = random.random()
-
                 glColor3f(1, 1, 0.9)
                 glVertex3fv(self.vertices[vertex])
         glEnd()
@@ -61,6 +55,35 @@ class Tunel:
                 glVertex3fv(self.vertices[vertex])
         glEnd()
 
+    def rotate(self, x):
+
+        cos_x = math.cos(math.pi / self.sides_num / 2 * x)
+        sin_x = math.sin(math.pi / self.sides_num / 2 * x)
+
+        rotation_matrix = np.array([[cos_x, -sin_x, 0],
+                                    [sin_x, cos_x, 0],
+                                    [0, 0, 1]])
+
+        for v in range(0, self.sides_num * self.depth):
+            self.vertices[v] = np.matmul(rotation_matrix, self.vertices[v])
+
+    def move(self):
+
+        length = len(self.vertices)
+
+        if self.vertices[length - 1][2] >= 400:
+
+            temp = []
+
+            for i in range(length - self.sides_num, length):
+                temp.append(self.vertices[length - self.sides_num])
+                self.vertices.pop(length - self.sides_num)
+            for i in range(0, self.sides_num):
+                temp[i][2] = -self.interval
+                self.vertices.insert(i, temp[i])
+        for v in range(0, self.sides_num * self.depth):
+            self.vertices[v] = np.add(self.vertices[v], [0, 0, 2])
+
 
 def main():
 
@@ -70,10 +93,10 @@ def main():
     pygame.init()
 
     win_size = (1200, 900)
-    pygame.display.set_mode(win_size, DOUBLEBUF | OPENGL)
+    pygame.display.set_mode(win_size, pygame.DOUBLEBUF | pygame.OPENGL)
 
-    gluPerspective(90, (win_size[0]/win_size[1]), 0.9, 500)
-    glTranslate(0, 0, -400)
+    gluPerspective(90, (win_size[0]/win_size[1]), 0.9, 400)
+    glTranslate(0, terrain.radius - 20, -400)
     # glEnable(GL_DEPTH_TEST)
 
     clock = pygame.time.Clock()
@@ -81,11 +104,19 @@ def main():
 
     while not exit_flag:
         clock.tick(60)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for eve in pygame.event.get():
+            if eve.type == pygame.QUIT:
                 exit_flag = True
 
-        glRotatef(1, 0, 0, int(2 * mat.pi))
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            terrain.rotate(-1)
+        if keys[pygame.K_a]:
+            terrain.rotate(1)
+
+        terrain.move()
+
+        # glRotatef(1, 0, 0, int(2 * mat.pi))
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         terrain.draw()
